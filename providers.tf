3c1d6b0f7c722e22e1808e5388ea53ea3b85de9c
anikulin <sanches00790@gmail.com>
2024-08-08 00:12:58 +0300

    hw-5 add lock-table

--- providers.tf
+++ providers.tf
@@ -1,3 +1,4 @@
+
 terraform {
   required_providers {
     yandex = {
@@ -5,6 +6,24 @@ terraform {
     }
   }
   required_version = "~>1.8.4"
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+      dynamodb = "https://docapi.serverless.yandexcloud.net/ru-central1/b1g3menglg4ufpjgjsho/etnemv24brddf55qd6d4"
+    }
+
+    bucket   = "tfstate-2a"
+    region   = "ru-central1"
+    key      = "terraform.tfstate"
+
+    skip_credentials_validation  = true
+    skip_region_validation       = true
+    skip_requesting_account_id   = true
+    skip_s3_checksum             = true
+
+    dynamodb_table = "tfstate-lock"
+  }
 }
 
 provider "yandex" {
